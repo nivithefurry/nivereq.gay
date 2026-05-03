f611416f3d84764920d0ec8159dc3fe0c51a07f3
nivithefurry <nivereq.media@gmail.com>
2026-05-03 03:02:07 +0200

    age script fixed

--- public/js/Utility/Age.js
+++ public/js/Utility/Age.js
@@ -5,5 +5,5 @@ function Age() {
   let bdayMs = new Date("2008-05-03").getTime();
   let currentMs = new Date().getTime();
   let difference = currentMs - bdayMs;
-  return Math.floor(difference / (1000 * 3600 * 24 * 365.25));
+  return Math.floor(difference / (1000 * 3600 * 24 * 365));
 }
